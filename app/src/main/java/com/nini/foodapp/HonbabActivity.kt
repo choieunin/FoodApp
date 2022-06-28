89d244761623a7beea5edf0f197a07e4130aef15
choeeun-in <cei8645@naver.com>
2022-06-28 09:54:31 +0900

    FoodApp

--- app/src/main/java/com/nini/foodapp/HonbabActivity.kt
+++ app/src/main/java/com/nini/foodapp/HonbabActivity.kt
@@ -3,9 +3,9 @@ package com.nini.foodapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HonbabActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_main)
+        setContentView(R.layout.layout_honbab)
     }
 }
