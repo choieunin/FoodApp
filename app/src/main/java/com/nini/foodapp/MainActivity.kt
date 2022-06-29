5575455790b05daace0948d7776daec8be9b9203
choeeun-in <cei8645@naver.com>
2022-06-29 16:10:34 +0900

    FoodApp

--- app/src/main/java/com/nini/foodapp/MainActivity.kt
+++ app/src/main/java/com/nini/foodapp/MainActivity.kt
@@ -1,2 +1,18 @@
 package com.nini.foodapp
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_main)
+        button1.setOnClickListener(ButtonListener())
+    }
+
+    inner class ButtonListener: View.OnClickListener {
+        override fun onClick(v.View?){
+        }
+    }
+}
