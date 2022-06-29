5f49ffec88b23c3677b3c47e53a96fd7f4e8a0ce
choeeun-in <cei8645@naver.com>
2022-06-29 15:33:58 +0900

    FoodApp

--- app/src/main/java/com/nini/foodapp/MainActivity.kt
+++ app/src/main/java/com/nini/foodapp/MainActivity.kt
@@ -1,22 +1,2 @@
 package com.nini.foodapp
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_main)
-        val button : Button = findViewById(R.id.button)
-        button.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                TODO("Not yet implemented")
-            }
-
-        })
-    }
-}
