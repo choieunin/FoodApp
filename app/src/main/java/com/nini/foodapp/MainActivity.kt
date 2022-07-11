d6c9b53ae9e7440963a559663f6ee40c11aa595c
choeeun-in <cei8645@naver.com>
2022-07-11 16:06:30 +0900

    FoodApp

--- app/src/main/java/com/nini/foodapp/MainActivity.kt
+++ app/src/main/java/com/nini/foodapp/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.nini.foodapp
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_main)
-        button1.setOnClickListener(ButtonListener())
     }
 
-    inner class ButtonListener: View.OnClickListener {
-        override fun onClick(v.View?){
-        }
-    }
+
 }
